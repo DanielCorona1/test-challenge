7313cffd081dc82ad0a4f0def128699308eecb8d
DanielCorona1 <dacoronacal@gmail.com>
2023-06-22 01:34:38 -0600

    Tarea zab

---
+++ tests/factorial.test.js
@@ -0,0 +1,21 @@
+const { default: expect } = require('expect');
+const factorial = require('./factorial');
+
+test('Factorial de 0 es 1', () => {
+  const resultado = factorial(0);
+  expect(resultado).toBe(1);
+});
+
+test('Factorial de 1 es 1', () => {
+  const resultado = factorial(1);
+  expect(resultado).toBe(1);
+});
+
+test('Factorial de 5 es 120', () => {
+  const resultado = factorial(5);
+  expect(resultado).toBe(120);
+});
+
+test('Factorial de número negativo arroja un error', () => {
+  expect(() => factorial(-1)).toThrow('El factorial solo está definido para números no negativos.');
+});
